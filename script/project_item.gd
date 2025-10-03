81b2be41feaa8f2bd1d837145cba801123d04591
Furau <3873153379@qq.com>
2025-10-03 22:12:33 +0800

    更新数据
    更新数据

---
+++ script/project_item.gd
@@ -0,0 +1,10 @@
+extends Button
+
+
+func _on_pressed() -> void:
+	var is_pressed = self.button_pressed
+	var first_child = self.get_child(0).text
+	print(is_pressed)
+	print(first_child)
+	
+	pass # Replace with function body.
